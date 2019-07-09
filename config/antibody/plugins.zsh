8fa9f5d003b6470a79967cf719e089b1c9dc5602
Brian Devins-Suresh <badevins@gmail.com>
2019-07-08 20:29:40 -0400

    Add more oh-my-zsh plugins

--- config/antibody/plugins.zsh
+++ config/antibody/plugins.zsh
@@ -1,6 +1,14 @@
 source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/lib/git.zsh
 source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/lib/theme-and-appearance.zsh
 source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/themes/jreese.zsh-theme
+source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/nvm/nvm.plugin.zsh
+fpath+=( /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/nvm )
+source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pyenv/pyenv.plugin.zsh
+fpath+=( /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pyenv )
+source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/rvm/rvm.plugin.zsh
+fpath+=( /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/rvm )
+source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/sdk/sdk.plugin.zsh
+fpath+=( /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/sdk )
 source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source /Users/bdevinssuresh/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
